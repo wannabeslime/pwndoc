16ebb4469b279d8422416a0df4cb82f0a79d3fb4
wannabeslime <kalkie_hide@yahoo.co.jp>
2022-02-16 01:49:29 +0900

    add_japanese

--- frontend/src/i18n/index.js
+++ frontend/src/i18n/index.js
@@ -2,10 +2,12 @@ import enUS from './en-US'
 import zhCN from './zh-CN'
 import deDE from './de-DE'
 import frFR from './fr-FR'
+import jaJP from './ja-JP'
 
 export default {
   'en-US': enUS,
   'zh-CN': zhCN,
   'de-DE': deDE,
-  'fr-FR': frFR
+  'fr-FR': frFR,
+  'ja-JP': jaJP
 } 
